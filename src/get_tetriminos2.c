490c4b04bb5282fc27b23a3d1e1b95cfec750d8b
wide-aze <wide-aze@users.noreply.github.com>
2015-12-16 15:07:19 +0100

    Update get_tetriminos2.c

--- src/get_tetriminos2.c
+++ src/get_tetriminos2.c
@@ -13,33 +13,33 @@
 #include "fillit.h"
 #include "libft.h"
 
-static void   check_cur_pos_tetri(t_env *env, int i, int *count)
+static voi		check_cur_pos_tetri(t_env *env, int i, int *count)
 {
-  if (*count < 4 && ((X < 3 && TETRI_CONTENT(i)[Y][X + 1] == '#')
-  || (X > 0 && TETRI_CONTENT(i)[Y][X - 1] == '#')
-  || (Y < 3 && TETRI_CONTENT(i)[Y + 1][X] == '#')
-  || (Y > 0 && TETRI_CONTENT(i)[Y - 1][X] == '#')))
-    *count = *count + 1;
-  else
-    ft_exit("error");
+	if (*count < 4 && ((X < 3 && TETRI_CONTENT(i)[Y][X + 1] == '#')
+	|| (X > 0 && TETRI_CONTENT(i)[Y][X - 1] == '#')
+	|| (Y < 3 && TETRI_CONTENT(i)[Y + 1][X] == '#')
+	|| (Y > 0 && TETRI_CONTENT(i)[Y - 1][X] == '#')))
+		*count = *count + 1;
+	else
+		ft_exit("error");
 }
 
-void      check_each_tetri_composition(t_env *env)
+void			check_each_tetri_composition(t_env *env)
 {
-  int   i;
-  int   count;
+	int		i;
+	int		count;
 
-  i = -1;
-  while (++i < NB_TETRI)
-  {
-    count = 0;
-    Y = -1;
-    while (++Y < 4)
-    {
-      X = -1;
-      while (++X < 4)
-        if (TETRI_CONTENT(i)[Y][X] == '#')
-          check_cur_pos_tetri(env, i, &count);
-    }
-  }
+	i = -1;
+	while (++i < NB_TETRI)
+	{
+		count = 0;
+		Y = -1;
+		while (++Y < 4)
+		{
+			X = -1;
+			while (++X < 4)
+				if (TETRI_CONTENT(i)[Y][X] == '#')
+					check_cur_pos_tetri(env, i, &count);
+		}
+	}
 }
