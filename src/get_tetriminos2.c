7072342bcef1ab3bca962c8682cf1d82635bb86c
wide-aze <wide-aze@student.42.fr>
2015-12-16 13:35:13 +0100

    Fixed # links verification

--- src/get_tetriminos2.c
+++ src/get_tetriminos2.c
@@ -37,25 +37,24 @@ static void		check_1tetri_compo(t_env *env, int i, int count, int must_exit)
 	int		last_y;
 
 	assign_last_x_y(-1, -1, &last_x, &last_y);
-	while (count < 4)
+	while (++count < 4)
 	{
-		if ((X != last_x || Y != last_y) && X >= 0 && Y >= 0 && X + 1 < 4
+		if ((X + 1 != last_x || Y != last_y) && X >= 0 && Y >= 0 && X + 1 < 4
 		&& Y < 4 && TETRI_CONTENT(i)[Y][X + 1] == '#')
 			assign_last_x_y(X++, Y, &last_x, &last_y);
-		else if ((X != last_x || Y != last_y) && X - 1 >= 0 && Y >= 0 && X < 4
+		else if ((X - 1 != last_x || Y != last_y) && X - 1 >= 0 && Y >= 0 && X < 4
 		&& Y < 4 && TETRI_CONTENT(i)[Y][X - 1] == '#')
 			assign_last_x_y(X--, Y, &last_x, &last_y);
-		else if ((X != last_x || Y != last_y) && X >= 0 && Y >= 0 && X < 4
+		else if ((X != last_x || Y + 1 != last_y) && X >= 0 && Y >= 0 && X < 4
 		&& Y + 1 < 4 && TETRI_CONTENT(i)[Y + 1][X] == '#')
 			assign_last_x_y(X, Y++, &last_x, &last_y);
-		else if ((X != last_x || Y != last_y) && X >= 0 && Y - 1 >= 0 && X < 4
+		else if ((X != last_x || Y - 1 != last_y) && X >= 0 && Y - 1 >= 0 && X < 4
 		&& Y < 4 && TETRI_CONTENT(i)[Y - 1][X] == '#')
 			assign_last_x_y(X, Y--, &last_x, &last_y);
 		else if (must_exit)
 			ft_exit("error");
 		else if (!must_exit)
 			return ;
-		count++;
 	}
 }
 
@@ -63,8 +62,6 @@ void			check_each_tetri_composition(t_env *env)
 {
 	int		i;
 
-	X = 0;
-	Y = 0;
 	i = -1;
 	while (++i < NB_TETRI)
 	{
