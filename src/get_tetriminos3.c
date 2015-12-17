47ba8c9ae0584a88fdf93dd1ebda06a4ef12ca9c
wide-aze <wide-aze@student.42.fr>
2015-12-17 13:46:21 +0100

    scotch

--- src/get_tetriminos3.c
+++ src/get_tetriminos3.c
@@ -25,15 +25,53 @@ static void		init_tabs(int *lines, int *col)
 	}
 }
 
+static void		fill_tabs(t_env *env, int tetri, int *lines, int *col)
+{
+  int     i;
+  int     j;
+  
+  i = -1;
+  while (++i < 4)
+  {
+    j = -1;
+    while (++j < 4)
+    {
+      if (TETRI_CONTENT(tetri)[i][j] == '#')
+      {
+      	col[j] = 1;
+      	lines[i] = 1;
+      }
+    }
+  }
+}
+
+static int 		search_first(int *tab)
+{
+	if (tab[0])
+		return (0);
+	if (tab[1])
+		return (1);
+	if (tab[2])
+		return (2);
+	return (3);
+}
+
 void			check_tetri3(t_env *env, int i, int nb_col, int nb_lines)
 {
 	int		lines[4];
 	int		col[4];
+	int		first;
 
 	init_tabs(&lines[0], &col[0]);
-	return;
-	(void)env;
-	(void)i;
-	(void)nb_lines;
-	(void)nb_col;
+	fill_tabs(env, i, &lines[0], &col[0]);
+	first = search_first(&col[0]);
+	if (nb_col == 2 && (first > 2 || !col[first + 1]))
+		ft_exit("error1");
+	if (nb_col == 3 && (first > 1 || !col[first + 1] || !col[first + 2]))
+		ft_exit("error2");
+	first = search_first(&lines[0]);
+	if (nb_lines == 2 && (first > 2 || !lines[first + 1]))
+		ft_exit("error3");
+	if (nb_lines == 3 && (first > 1 || !lines[first + 1] || !lines[first + 2]))
+		ft_exit("error4");
 }
